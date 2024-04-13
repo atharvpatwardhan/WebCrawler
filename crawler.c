ccbce6fcbe80b012cb683f7909600f7fb186577f
wolfy5786 <makhijaniankush24@gmail.com>
2024-04-13 16:28:40 -0400

    API

--- crawler.c
+++ crawler.c
@@ -1,5 +1,32 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+
+// Define a structure for queue elements.
+typedef struct URLQueueNode {
+    char *url;
+    struct URLQueueNode *next;
+} URLQueueNode;
+
+// Define a structure for a thread-safe queue.
+typedef struct {
+    URLQueueNode *head, *tail;
+    pthread_mutex_t lock;
+} URLQueue;
+
+
+void initQueue(URLQueue *queue);
+char *dequeue(URLQueue *queue);
+void enqueue(URLQueue *queue, const char *url);
+
+// Placeholder for the function to fetch and process a URL.
+void *fetch_url(void *arg);
+
+
 
 int main(int argc, char ** argv)
 {
