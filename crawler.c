a23b81caea51d0662535a576082d43985b79a6ca
Diya Kithani <diyakithani.2003@gmail.com>
2024-04-21 14:06:46 -0400

    implemented url filter+  depth control

--- crawler.c
+++ crawler.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <curl/curl.h>
+#include <stdbool.h>
 
 // Okay
 // Define a structure for queue elements.
@@ -29,6 +30,9 @@ typedef struct // Declaring Response struct
   size_t size;
 } Response;
 
+char *seed_domain;
+int depth_limit;
+
 size_t write_chunk(void *data, size_t size, size_t nmemb, void *userdata);
 void initQueue(URLQueue *queue);
 char *dequeue(URLQueue *queue);
@@ -37,20 +41,16 @@ void enqueue(URLQueue *queue, const char *url);
 // Placeholder for the function to fetch and process a URL.
 void *fetch_url(void *url);
 
-URLQueueNode* creteURLQueueNode(char* url)
+URLQueueNode *creteURLQueueNode(char *url)
 {
-  URLQueueNode* node = malloc(sizeof(URLQueueNode));
-  node->url = malloc(strlen(url)+1);
-  strcpy(node->url,url);
+  URLQueueNode *node = malloc(sizeof(URLQueueNode));
+  node->url = malloc(strlen(url) + 1);
+  strcpy(node->url, url);
   node->next = NULL;
   node->parent = NULL;
   node->depth = 0;
   return node;
 }
-
-
-
-
 
 size_t write_chunk(void *data, size_t size, size_t nmemb, void *userdata)
 {
@@ -77,7 +77,7 @@ void extract_url(char *html)
   sub = strstr(html, "href=\"http");
   if (sub == NULL)
   {
-    //printf("Boo hoo not working\n"); // implement while or remove entirely
+    // printf("Boo hoo not working\n"); // implement while or remove entirely
     return;
   }
   else
@@ -105,7 +105,6 @@ void extract_url(char *html)
     extract_url(html);
   }
 }
-
 void *fetchurl(char *url) // fetches url in response struct
 {
   CURL *curl;              // declaring handle
@@ -159,6 +158,26 @@ void logURL(const char *url)
 
   fclose(file); // close the file
 }
+bool url_filter(URLQueueNode *node)
+{
+  if (strstr(node->url, seed_domain) != NULL)
+  {
+    // Check if the depth is less than depth_limit
+    if (node->depth < depth_limit)
+    {
+      return true; // URL is valid
+    }
+    else
+    {
+      return false; // URL depth exceeds limit
+    }
+  }
+  else
+  {
+    return false; // URL does not belong to seed domain
+  }
+}
+
 int main(int argc, char **argv)
 {
   if (argc < 2)
@@ -166,11 +185,9 @@ int main(int argc, char **argv)
     printf("not enough args\n");
     return EXIT_SUCCESS;
   }
-
   char *url = argv[1]; // setting url as the first argument
-  printf("%s\n", url);
-
+  seed_domain = argv[1];
+  depth_limit = 5;
   fetchurl(argv[1]); // calling fetchurl on first argument
-
   return EXIT_SUCCESS;
 }
